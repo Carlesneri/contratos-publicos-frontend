e633c85eb669dee4210d29394ea893c1d89e2028
carlos.company <carlos.company@nexum.com>
2024-11-24 18:23:58 +0100

    Search page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ export function Header() {
 	return (
 		<header className="h-full">
 			<nav className="container mx-auto max-w-5xl px-4 text-4xl font-bold my-8">
-				Contratos Públicos
+				<a href="/">Contratos Públicos</a>
 			</nav>
 		</header>
 	)
