94ade5b5b846b4b14f595c160ea8f0da1a6dd17a
carlos.company <carlos.company@nexum.com>
2024-11-29 20:28:35 +0100

    Fix link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
 		<header className="h-full">
 			<nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
 				<div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
-					<a
+					<Link
 						href="/"
 						className="flex items-center space-x-3 rtl:space-x-reverse"
 					>
@@ -13,17 +13,17 @@ export function Header() {
 						<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
 							Contratos Públicos
 						</span>
-					</a>
+					</Link>
 					<div className="" id="navbar-dropdown">
 						<ul className="flex font-medium p-0 border-gray-100 rounded-lg space-x-8 rtl:space-x-reverse flex-row mt-0 border-0 bg-white dark:bg-gray-900 dark:border-gray-700">
 							<li>
-								<a
-									href="#"
+								<Link
+									href="/"
 									className="block py-2 px-3 text-gray-800 rounded bg-transparent p-0"
 									aria-current="page"
 								>
 									Home
-								</a>
+								</Link>
 							</li>
 						</ul>
 					</div>
